f1ad1af8ba354613484b4bad36877baea1488941
Sergey Kazantsev <kazsergkaz@gmail.com>
2026-07-16 20:23:09 +0300

    fix writing logs in log file

--- src/logger.py
+++ src/logger.py
@@ -11,6 +11,7 @@ def init_logger():
         return logging.getLogger('logs')
 
     log_file = Path(__file__).resolve().parent.parent / settings.log_file
+    log_file.parent.mkdir(parents=True, exist_ok=True)
 
     logging.basicConfig(
         level=settings.log_level.upper(),
